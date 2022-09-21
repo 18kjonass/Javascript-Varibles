34863e979e685929d4ab2e11191f4a02e99d937c
18kjonass <112894153+18kjonass@users.noreply.github.com>
2022-09-21 19:37:44 +0100

    Add files via upload

---
+++ javascript math.js
@@ -0,0 +1,28 @@
+const PI = 3.14;
+let r = 2
+let A = PI*(r**2) 
+console.log(A)
+
+let l = 3;
+let w = 5;
+let p = 5*3
+console.log(p)
+
+let B = 8
+let h = 2
+let t = 1/2*B*h
+console.log(t)
+
+let a = 4
+let b = 6
+let c = a*a+b*b
+console.log(c)
+
+let v = l*w*h
+console.log(v)
+
+let V = (r*r)*h*PI;
+console.log(V)
+
+let vs = (r*r*r)*PI*3/4
+console.log(vs)
